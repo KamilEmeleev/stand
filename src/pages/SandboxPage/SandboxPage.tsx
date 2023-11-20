09c516e52ec309a3e03ed46d2a67190618548f8e
emeleevke <emeleev@akbars.ru>
2023-11-20 17:46:44 +0300

    check the locale

--- src/pages/SandboxPage/SandboxPage.tsx
+++ src/pages/SandboxPage/SandboxPage.tsx
@@ -1,5 +1,9 @@
+import { useMemo } from 'react';
+
 import { BanIcon } from '@ozen-ui/icons';
 import { Button } from '@ozen-ui/kit/Button';
+import { FilePicker } from '@ozen-ui/kit/FilePicker';
+import { enUS } from '@ozen-ui/kit/locale';
 import { Stack } from '@ozen-ui/kit/Stack';
 import {
   extendTheme,
@@ -7,24 +11,21 @@ import {
   ThemeProvider,
 } from '@ozen-ui/kit/ThemeProvider';
 import { Tooltip } from '@ozen-ui/kit/Tooltip';
+import { Typography } from '@ozen-ui/kit/Typography';
 
 export const SandboxPage = () => {
+  const customTheme = useMemo(() => {
+    const themeWithLocale = extendTheme(themeOzenDefault, enUS);
+
+    return extendTheme(themeWithLocale, {
+      defaultProps: { Button: { size: 's', variant: 'outlined' } },
+    });
+  }, []);
+
   return (
-    <Stack gap="l">
-      <Button
-        iconLeft={() => (
-          <Tooltip label="test">
-            <BanIcon />
-          </Tooltip>
-        )}
-      >
-        Btn
-      </Button>
-      <ThemeProvider
-        theme={extendTheme(themeOzenDefault, {
-          defaultProps: { Button: { size: 's', variant: 'outlined' } },
-        })}
-      >
+    <ThemeProvider theme={customTheme}>
+      <Stack gap="l" direction="column" align="start">
+        <Typography variant="text-l_1">Custom theme</Typography>
         <Button
           iconLeft={() => (
             <Tooltip label="test">
@@ -34,7 +35,8 @@ export const SandboxPage = () => {
         >
           Btn
         </Button>
-      </ThemeProvider>
-    </Stack>
+        <FilePicker />
+      </Stack>
+    </ThemeProvider>
   );
 };
