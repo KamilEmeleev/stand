854790a8b7760c1e3df6cf34e647005c5e7c410c
emeleevke <emeleev@akbars.ru>
2024-03-21 09:51:39 +0300

    add an example with Calendar

--- src/widgets/CalendarWidget/CalendarWidget.tsx
+++ src/widgets/CalendarWidget/CalendarWidget.tsx
@@ -1,10 +1,12 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 
 import { ArrowRightIcon, CalendarIcon } from '@ozen-ui/icons';
 import { Button } from '@ozen-ui/kit/ButtonNext';
+import { Calendar } from '@ozen-ui/kit/Calendar';
 import { Divider } from '@ozen-ui/kit/Divider';
 import { IconButton } from '@ozen-ui/kit/IconButtonNext';
 import { List, ListItem, ListItemIcon, ListItemText } from '@ozen-ui/kit/List';
+import { Popover } from '@ozen-ui/kit/Popover';
 import { Stack } from '@ozen-ui/kit/Stack';
 import { Typography } from '@ozen-ui/kit/Typography';
 
@@ -15,6 +17,17 @@ import s from './CalendarWidget.module.css';
 import { formatTime } from './utils';
 
 export const CalendarWidget = () => {
+  const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <Stack direction="column" gap="l" align="start" fullWidth>
       <Typography variant="text-xl_1">Календарь событий</Typography>
@@ -35,13 +48,27 @@ export const CalendarWidget = () => {
                   secondary={formatTime(date.from, date.to)}
                 />
                 <ListItemIcon>
-                  <IconButton icon={CalendarIcon} rounded compressed />
+                  <IconButton
+                    icon={CalendarIcon}
+                    onClick={handleClick}
+                    rounded
+                    compressed
+                  />
                 </ListItemIcon>
               </ListItem>
             </Fragment>
           );
         })}
       </List>
+      <Popover
+        open={open}
+        anchorRef={{ current: anchorEl }}
+        onClose={handleClose}
+        placement="bottom-start"
+        offset={[0, 8]}
+      >
+        <Calendar />
+      </Popover>
       <Button variant="function" iconRight={<ArrowRightIcon size="s" />}>
         Все события календаря
       </Button>
