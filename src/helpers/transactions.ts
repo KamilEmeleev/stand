75e8171d00c80c19e7fa23d30ef83f89805327b3
emeleevke <emeleev@akbars.ru>
2024-04-26 10:44:11 +0300

    add brand icons to FinancialTransactionsWidget

--- src/helpers/transactions.ts
+++ src/helpers/transactions.ts
@@ -1,5 +1,12 @@
 import { ReactElement } from 'react';
 
+import {
+  BrApplePayColoredIcon,
+  BrCardMastercardColoredIcon,
+  BrCardVisaColoredIcon,
+  BrGooglePayDarkColoredIcon,
+} from '@ozen-ui/icons';
+
 export enum TransactionType {
   success = 'success',
   declined = 'declined',
@@ -9,41 +16,73 @@ export type TransactionTypes = {
   [key in TransactionType]: ReactElement;
 };
 
+export enum PaymentMethodType {
+  visa = 'visa',
+  mastercard = 'mastercard',
+  'google-pay' = 'google-pay',
+  'apple-pay' = 'apple-pay',
+}
+
+export const paymentMethodsIcons = {
+  [PaymentMethodType.visa]: BrCardVisaColoredIcon,
+  [PaymentMethodType.mastercard]: BrCardMastercardColoredIcon,
+  [PaymentMethodType['google-pay']]: BrGooglePayDarkColoredIcon,
+  [PaymentMethodType['apple-pay']]: BrApplePayColoredIcon,
+};
+
 export const transactions = [
   {
     type: TransactionType.success,
     name: {
-      first: 'Операция 1',
+      first: 'Ord-01',
       second: 'Списание',
     },
     date: '27.08.2023',
     amount: -9500.0,
+    paymentMethod: {
+      type: PaymentMethodType.visa,
+      name: 'Visa',
+      number: '**** 4567',
+    },
   },
   {
     type: TransactionType.success,
     name: {
-      first: 'Операция 2',
+      first: 'Ord-02',
       second: 'Списание',
     },
     date: '27.08.2023',
     amount: -16500.0,
+    paymentMethod: {
+      type: PaymentMethodType['apple-pay'],
+      name: 'Apple Pay',
+    },
   },
   {
     type: TransactionType.success,
     name: {
-      first: 'Операция 3',
+      first: 'Ord-03',
       second: 'Пополнение',
     },
     date: '03.05.2023',
     amount: 546400.0,
+    paymentMethod: {
+      type: PaymentMethodType['google-pay'],
+      name: 'Google Pay',
+    },
   },
   {
     type: TransactionType.declined,
     name: {
-      first: 'Операция 4',
+      first: 'Ord-04',
       second: 'Пополнение',
     },
     date: '06.04.2023',
     amount: 17450.0,
+    paymentMethod: {
+      type: PaymentMethodType.mastercard,
+      name: 'Mastercard',
+      number: '**** 9011',
+    },
   },
 ];
--- src/widgets/FinancialTransactionsWidget/FinancialTransactionsWidget.tsx
+++ src/widgets/FinancialTransactionsWidget/FinancialTransactionsWidget.tsx
@@ -11,13 +11,18 @@ import {
 import { Tag } from '@ozen-ui/kit/Tag';
 import { Typography } from '@ozen-ui/kit/Typography';
 
-import { transactions, TransactionTypes } from '../../helpers';
+import {
+  paymentMethodsIcons,
+  transactions,
+  TransactionTypes,
+} from '../../helpers';
 
+import s from './FinancialTransactionsWidget.module.css';
 import { sum } from './utils';
 
 const transactionTypes: TransactionTypes = {
   success: <Tag variant="action" label="Исполнена" size="s" />,
-  declined: <Tag variant="warning" label="Отклонена" size="s" />,
+  declined: <Tag variant="error" label="Отклонена" size="s" />,
 };
 
 export const FinancialTransactionsWidget = () => {
@@ -38,37 +43,55 @@ export const FinancialTransactionsWidget = () => {
           <TableHead>
             <TableRow>
               <TableCell align="left">Наименование</TableCell>
-              <TableCell align="left">Статус</TableCell>
+              <TableCell align="left">Способ оплаты</TableCell>
               <TableCell align="left">Дата</TableCell>
               <TableCell align="left">Сумма</TableCell>
+              <TableCell align="left">Статус</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {transactions.map(({ type, name, date, amount }, index) => (
-              <TableRow key={index} hover>
-                <TableCell>
-                  <Typography variant="text-m" noWrap>
-                    {name.first}
-                  </Typography>
-                  <Typography variant="text-s" color="secondary" noWrap>
-                    {name.second}
-                  </Typography>
-                </TableCell>
-                <TableCell>{transactionTypes[type]}</TableCell>
-                <TableCell>
-                  <Typography variant="text-m">{date}</Typography>
-                </TableCell>
-                <TableCell>
-                  <Typography
-                    variant="text-m_1"
-                    color={amount > 0 ? 'success' : 'error'}
-                    noWrap
-                  >
-                    {sum(amount)}
-                  </Typography>
-                </TableCell>
-              </TableRow>
-            ))}
+            {transactions.map(
+              ({ type, name, date, paymentMethod, amount }, index) => {
+                const Icon = paymentMethodsIcons[paymentMethod.type];
+
+                return (
+                  <TableRow key={index} hover>
+                    <TableCell>
+                      <Typography variant="text-m" noWrap>
+                        {name.first}
+                      </Typography>
+                      <Typography variant="text-s" color="secondary" noWrap>
+                        {name.second}
+                      </Typography>
+                    </TableCell>
+                    <TableCell>
+                      <Stack gap="m" align="center">
+                        <div className={s.paymentMethod}>
+                          <Icon />
+                        </div>
+                        <Stack direction="column">
+                          <Typography variant="text-s">
+                            {paymentMethod.name}
+                          </Typography>
+                          <Typography variant="text-s" color="secondary">
+                            {paymentMethod.number}
+                          </Typography>
+                        </Stack>
+                      </Stack>
+                    </TableCell>
+                    <TableCell>
+                      <Typography variant="text-m">{date}</Typography>
+                    </TableCell>
+                    <TableCell>
+                      <Typography variant="text-m_1" noWrap>
+                        {sum(amount)}
+                      </Typography>
+                    </TableCell>
+                    <TableCell>{transactionTypes[type]}</TableCell>
+                  </TableRow>
+                );
+              }
+            )}
           </TableBody>
         </Table>
       </TableContainer>
