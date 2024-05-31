acd8ee029c20fa42b194528e786e6e1284d0dddc
emeleevke <emeleev@akbars.ru>
2024-05-31 14:31:23 +0300

    improve the getTime util

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -20,5 +20,7 @@ export function isToday(day: Date) {
 }
 
 export function getTime(date: Date) {
-  return `${date.getHours()}:${String(date.getMinutes()).padStart(2, '0')}`;
+  return `${String(date.getHours()).padStart(2, '0')}:${String(
+    date.getMinutes()
+  ).padStart(2, '0')}`;
 }
