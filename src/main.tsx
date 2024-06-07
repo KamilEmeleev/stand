98c3c25245f0782a9124d18970d34a652e0bfd17
emeleevke <emeleev@akbars.ru>
2024-06-07 10:45:58 +0300

    Revert "add a polyfill for kk locale"
    This reverts commit de9dbd7031a03234308ac8e6d0de82f227490fad.

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,4 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-import '@formatjs/intl-datetimeformat/polyfill-force.js';
-import '@formatjs/intl-datetimeformat/locale-data/kk';
-import '@formatjs/intl-datetimeformat/locale-data/ru';
-import '@formatjs/intl-datetimeformat/locale-data/en';
-import '@formatjs/intl-datetimeformat/add-all-tz';
-
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
